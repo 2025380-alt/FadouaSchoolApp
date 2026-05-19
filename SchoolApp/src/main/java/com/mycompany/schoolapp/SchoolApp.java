63d85fa8ba685bbd5ba9f2a214b4d7b922ab7144
fadouaelbakar-sketch <fadoua.el.bakar@gmail.com>
2026-05-20 00:32:49 +0200

    Created schoolApp.java

---
+++ SchoolApp/src/main/java/com/mycompany/schoolapp/SchoolApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.schoolapp;
+
+/**
+ *
+ * @author store one
+ */
+public class SchoolApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
