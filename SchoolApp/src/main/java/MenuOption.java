bdc0fae5cf3d0fdfa69eeb9e46b67bf1cd491d63
fadouaelbakar-sketch <fadoua.el.bakar@gmail.com>
2026-05-20 00:39:11 +0200

    (MenuOption): add SORT and SEARCH constants

--- SchoolApp/src/main/java/MenuOption.java
+++ SchoolApp/src/main/java/MenuOption.java
@@ -8,3 +8,17 @@
  * @author store one
  */
 public enum MenuOption {
+
+    SORT(1, "Sort a Dummy List of People"),
+    SEARCH(2, "Search in the List and Return Relevant Information"),
+    ADD_RECORD(3, "Add Records"),
+    CREATE_BINARY_TREE(4, "Create Binary Tree"),
+    EXIT(5, "Exit");
+
+    private final int option;          // numeric code for menu selection
+    private final String description;   // human‑readable description
+
+    MenuOption(int option, String description) {
+        this.option = option;
+        this.description = description;
+    }
