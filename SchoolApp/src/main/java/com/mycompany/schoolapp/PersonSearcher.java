15f29cc4779865a0c69aaa7efa7a43966d3f0d37
fadouaelbakar-sketch <fadoua.el.bakar@gmail.com>
2026-05-20 21:19:01 +0200

    set up binarySearch method

--- SchoolApp/src/main/java/com/mycompany/schoolapp/PersonSearcher.java
+++ SchoolApp/src/main/java/com/mycompany/schoolapp/PersonSearcher.java
@@ -10,6 +10,15 @@ package com.mycompany.schoolapp;
  */
 public class PersonSearcher {
     
-    
+      /**
+     * Recursively searches for a person with the given full name.
+     * @param list sorted ArrayList of Person objects
+     * @param fullName the name to search for (case‑insensitive)
+     * @param low lower bound index (inclusive)
+     * @param high upper bound index (inclusive)
+     * @return index of the found Person, or -1 if not found
+     */
+    public static int binarySearch(ArrayList<Person> list, String fullName,
+                                   int low, int high) {
     
 }
